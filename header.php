4e526f180af1a8c98901ee6d06f9ac0c47b92bf0
hosseinshaker <hosseinshaker78@gmail.com>
2023-08-07 01:11:47 +0330

    v1.0.0.1

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <html dir="<?php bloginfo('text_direction') ?>" lang="<?php bloginfo('language') ?>">
 <head>
-    <title>صفحه اصلی</title>
+    <?php
+wp_head();
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1">
---
+++ inc/front/styled.php
@@ -0,0 +1,9 @@
+<?PHP
+wp_enqueue_style('grid-gallery.min',get_template_directory_uri().'/Css/grid-gallery.min.css');
+wp_enqueue_style('main',get_template_directory_uri().'/Css/Main.css');
+wp_enqueue_style('menu',get_template_directory_uri().'/Css/Menu.css');
+wp_enqueue_style('style',get_template_directory_uri().'/Css/style.css');
+wp_enqueue_style('owl.theme.min',get_template_directory_uri().'/Css/owl.theme.min.css');
+wp_enqueue_style('owl.carousel.min',get_template_directory_uri().'/Css/owl.carousel.min.css');
+
+?>

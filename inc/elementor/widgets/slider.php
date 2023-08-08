9dd23c0bd96c1b6f007078a9368ff412023e406a
Shaker <hosseinshaker78@gmail.com>
2023-08-08 13:36:54 +0330

    123

--- inc/elementor/widgets/slider.php
+++ inc/elementor/widgets/slider.php
@@ -34,29 +34,6 @@ class Yekta_Web_Slider extends \Elementor\Widget_Base {
 		);
 
 
-		$repeater = new \Elementor\Repeater();
-
-
-		$repeater->add_control(
-			'slide_url',
-			[
-				'label' => 'لینک',
-				'type' => \Elementor\Controls_Manager::URL,
-				'label_block' => true,
-			]
-		);
-
-		$repeater->add_control(
-			'slide_image',
-			[
-				'label' => 'تصویر',
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'label_block' => true,
-			]
-		);
-
-		/*
-
 		$this->add_control(
 			'slideryektaweb',
 			[
@@ -80,8 +57,6 @@ class Yekta_Web_Slider extends \Elementor\Widget_Base {
 
 			]
 		);
-
-		*/
 		$this->end_controls_section();
 
 		// Content Tab End
@@ -118,9 +93,45 @@ class Yekta_Web_Slider extends \Elementor\Widget_Base {
 		$settings = $this->get_settings_for_display();
 		?>
 
+       <!--Slider-->
+       <section class="container">
+        <div class="row">
+
+            <div class="col-lg-12 p-0 mb-3">
+                <div id="owl-mainslider" class="owl-carousel owl-theme text-center">
+<?php
+if ( $settings['slideryektaweb'] ) {
+	foreach (  $settings['slideryektaweb'] as $item ) {
+		//echo $item['link_slider'];
+		$image_slider=$item['image_slider_yektaweb']['url'];
+		echo '<div class="item"><a href="'.$item['link_slider'].'"> <img  class="img-fluid radius-slide" src="'.$image_slider.'"/></a></div>';
+	}
+}
+
+?>
+                </div>
+            </div>
+        </div>
+    </section>
+       <!--End--Slider-->
+
 
 		<?php
 	}
 
+	protected function content_template() {
+		?>
+		<# if ( settings.slideryektaweb.length ) { #>
+			<dl>
+			<# _.each( settings.slideryektaweb, function( item ) { #>
+				<!--<dt class="elementor-repeater-item-{{ item._id }}">{{{ item.link_slider }}}</dt>-->
+				<dd>
+					<img src="{{{ item.image_slider_yektaweb.url }}}">
+				</dd>
+			<# }); #>
+			</dl>
+		<# } #>
+		<?php
+	}
 
 }

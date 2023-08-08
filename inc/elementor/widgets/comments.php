bb98407d16245ce4a85f3fc0680ab8e4b560ffe2
Hossein Shaker <hosseinshaker78@gmail.com>
2023-08-08 16:37:09 +0330

    3
    3

--- inc/elementor/widgets/comments.php
+++ inc/elementor/widgets/comments.php
@@ -6,7 +6,7 @@ class Yekta_Web_Comments extends \Elementor\Widget_Base {
 	}
 
 	public function get_title() {
-		return esc_html__( 'کامنت مشتری', 'elementor-addon');
+		return esc_html__( 'اسلایدر یکتاوب', 'elementor-addon' );
 	}
 
 	public function get_icon() {
@@ -18,7 +18,7 @@ class Yekta_Web_Comments extends \Elementor\Widget_Base {
 	}
 
 	public function get_keywords() {
-		return [ 'جعبه', 'دسته' ];
+		return [ 'اسلایدر', 'slider' ];
 	}
 
 	protected function register_controls() {
@@ -28,48 +28,35 @@ class Yekta_Web_Comments extends \Elementor\Widget_Base {
 		$this->start_controls_section(
 			'section_title',
 			[
-				'label' => esc_html__( 'انتخاب دسته/ جعبه یکتاوب', 'elementor-addon' ),
+				'label' => esc_html__( 'اسلایدر یکتاوب', 'elementor-addon' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
 
-        $this->add_control(
-			'box_category_title',
+
+		$this->add_control(
+			'slideryektaweb',
 			[
-				'label' => esc_html__( 'عنوان', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'عنوان پیشفرض', 'textdomain' ),
-				'placeholder' => esc_html__( 'عنوان خود را اینجا وارد کنید', 'textdomain' ),
-			]
-		);
-        $this->add_control(
-			'box_category_url',
-			[
-				'label' => esc_html__( 'لینک', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::URL,
-				'options' => [ 'url', 'is_external', 'nofollow' ],
-				'default' => [
-					'url' => '',
-					'is_external' => true,
-					'nofollow' => true,
-					// 'custom_attributes' => '',
+				'label' => esc_html__( 'اسلایدر یکتاوب', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => [
+					[
+						'name' => 'link_slider',
+						'label' => esc_html__( 'لینک اسلاید', 'textdomain' ),
+						'type' => \Elementor\Controls_Manager::TEXT,
+						'label_block' => true,
+					],
+					[	'name' => 'image_slider_yektaweb',
+						'label' => esc_html__( 'انتخاب عکس اسلاید', 'textdomain' ),
+						'type' => \Elementor\Controls_Manager::MEDIA,
+						'default' => [
+							'url' => \Elementor\Utils::get_placeholder_image_src(),
+						],
+					],
 				],
-				'label_block' => true,
+
 			]
 		);
-        $this->add_control(
-			'box_category_image',
-			[
-				'label' => esc_html__( 'انتخاب عکس', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
-				],
-			]
-		);
-
-
-
 		$this->end_controls_section();
 
 		// Content Tab End
@@ -97,6 +84,7 @@ class Yekta_Web_Comments extends \Elementor\Widget_Base {
 		);
 
 		$this->end_controls_section();
+
 		// Style Tab End
 
 	}
@@ -104,20 +92,45 @@ class Yekta_Web_Comments extends \Elementor\Widget_Base {
 	protected function render() {
 		$settings = $this->get_settings_for_display();
 		?>
-    <!--Category-product-->
-    <div class="py-4 p-3 d-flex align-items-center justify-content-around algin text-lg-right text-md-center mb-3 service_style category-style radius30">
-        <img src="<?php echo $settings['box_category_image']['url'];?>" width="100px" height="100px">
-            <h3 class="YekanBakhFaNum-Bold fa1-2"><?php echo $settings['box_category_title'];?></h3>
-            <a href="<?php echo $settings['box_category_url']['url'];?>">
-                <div class="arrow-box d-flex align-items-center justify-content-center">
-                <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10.8019 6.17453H2.56566C2.00285 6.17453 1.53613 5.70782 1.53613 5.14501C1.53613 4.5822 2.00285 4.11548 2.56566 4.11548H10.8019C11.3647 4.11548 11.8314 4.5822 11.8314 5.14501C11.8314 5.70782 11.3647 6.17453 10.8019 6.17453Z" fill="#292D32"/>
-                    <path d="M5.31096 10.2918C5.05014 10.2918 4.78933 10.1958 4.58342 9.98985L0.465311 5.87174C0.067227 5.47366 0.067227 4.81476 0.465311 4.41667L4.58342 0.298563C4.98151 -0.099521 5.64041 -0.099521 6.03849 0.298563C6.43657 0.696647 6.43657 1.35554 6.03849 1.75363L2.64791 5.14421L6.03849 8.53479C6.43657 8.93287 6.43657 9.59177 6.03849 9.98985C5.83258 10.1958 5.57177 10.2918 5.31096 10.2918Z" fill="#292D32"/>
-                </svg>
-                 </div>
-            </a>
-    </div>
-    <!--End--Category-product-->
+
+       <!--Slider-->
+       <section class="container">
+        <div class="row">
+
+            <div class="col-lg-12 p-0 mb-3">
+                <div id="owl-mainslider" class="owl-carousel owl-theme text-center">
+<?php
+if ( $settings['slideryektaweb'] ) {
+	foreach (  $settings['slideryektaweb'] as $item ) {
+		//echo $item['link_slider'];
+		$image_slider=$item['image_slider_yektaweb']['url'];
+		echo '<div class="item"><a href="'.$item['link_slider'].'"> <img  class="img-fluid radius-slide" src="'.$image_slider.'"/></a></div>';
+	}
+}
+
+?>
+                </div>
+            </div>
+        </div>
+    </section>
+       <!--End--Slider-->
+
+
+		<?php
+	}
+
+	protected function content_template() {
+		?>
+		<# if ( settings.slideryektaweb.length ) { #>
+			<dl>
+			<# _.each( settings.slideryektaweb, function( item ) { #>
+				<!--<dt class="elementor-repeater-item-{{ item._id }}">{{{ item.link_slider }}}</dt>-->
+				<dd>
+					<img src="{{{ item.image_slider_yektaweb.url }}}">
+				</dd>
+			<# }); #>
+			</dl>
+		<# } #>
 		<?php
 	}
 

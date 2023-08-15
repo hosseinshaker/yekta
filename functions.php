2c52eb9a4db6251cfa0e8ffc2a8c92fbe333f6d5
Hossein Shaker <hosseinshaker78@gmail.com>
2023-08-15 16:04:28 +0330

    abc asd

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
-add_theme_support( 'post-thumbnails' );//register image support
+function yekta_prodocut_setup_theme(){
+    add_theme_support( 'post-thumbnails' );//register image support
+    add_theme_support( 'woocommerce' );//register woocommerce support
+    add_theme_support( 'wc-product-gallery-zoom' );//zoom prodouct
+    add_theme_support( 'wc-product-gallery-lightbox' );//lightbox register
+    add_theme_support( 'wc-product-gallery-slider' );//slider gallery products
+}
+add_action('after_setup_theme','yekta_prodocut_setup_theme');
+
 get_template_part('inc/front/styled');//styled front load
 get_template_part('inc/front/scripted');//scripts front load
 get_template_part('inc/options');//scripts front load
